583756a09ac1bf3c1c74da6e8ba13daa28a02b80
vivekkumarjain09@gmail.com <vivekkumarjain09@gmail.com>
2020-09-06 22:09:52 +0530

    Given Test Data Setup and execution

--- PromotionEngine/PromotionEngine/Program.cs
+++ PromotionEngine/PromotionEngine/Program.cs
@@ -12,15 +12,34 @@ namespace PromotionEngine
         {
             Console.WriteLine("*** Scenario A ***");
             Order order = new Order(Guid.NewGuid());
-
-            order.Add(new LineItem(DataSetHelper.GetProductBySKU("A"), 7));
+            order.Add(new LineItem(DataSetHelper.GetProductBySKU("A"), 1));
             order.Add(new LineItem(DataSetHelper.GetProductBySKU("B"), 1));
             order.Add(new LineItem(DataSetHelper.GetProductBySKU("C"), 1));
             DataSetHelper.AddAllPromosToOrder(order);
 
-            var output = order.CalculateOrderPrice();
+            order.CalculateOrderPrice();
+
+            Console.WriteLine("*** Scenario B ***");
+            order = new Order(Guid.NewGuid());
+            order.Add(new LineItem(DataSetHelper.GetProductBySKU("A"), 5));
+            order.Add(new LineItem(DataSetHelper.GetProductBySKU("B"), 5));
+            order.Add(new LineItem(DataSetHelper.GetProductBySKU("C"), 1));
+            DataSetHelper.AddAllPromosToOrder(order);
+
+            order.CalculateOrderPrice();
+
+            Console.WriteLine("*** Scenario C ***");
+            order = new Order(Guid.NewGuid());
+            order.Add(new LineItem(DataSetHelper.GetProductBySKU("A"), 3));
+            order.Add(new LineItem(DataSetHelper.GetProductBySKU("B"), 5));
+            order.Add(new LineItem(DataSetHelper.GetProductBySKU("C"), 1));
+            order.Add(new LineItem(DataSetHelper.GetProductBySKU("D"), 1));
+            DataSetHelper.AddAllPromosToOrder(order);
+
+            order.CalculateOrderPrice();
 
             Console.ReadLine();
+
         }
     }
 }
