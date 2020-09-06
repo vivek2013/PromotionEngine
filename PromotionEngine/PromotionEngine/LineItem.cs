6d41b1a7f2e72851d222cddfd9657b4f5ba90797
vivekkumarjain09@gmail.com <vivekkumarjain09@gmail.com>
2020-09-06 21:01:40 +0530

    POCO classes for Product and LineItem

---
+++ PromotionEngine/PromotionEngine/LineItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine
+{
+    public class LineItem : Product
+    {
+        public LineItem(Product product, int quantity) : base(product)
+        {
+            if (quantity <= 0)
+            {
+                throw new ArgumentException("Quantity should be greater than zero.", nameof(quantity));
+            }
+
+            this.Quantity = quantity;
+        }
+
+        public int Quantity { get; set; }
+
+    }
+}
---
+++ PromotionEngine/PromotionEngine/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine
+{
+    public class Product
+    {
+        public Product(Product product) : this(product.Id, product.SKU, product.UnitPrice)
+        {
+
+        }
+        public Product(Guid id, string sku, decimal unitPrice)
+        {
+            if (id == Guid.Empty)
+            {
+                throw new ArgumentException("Can not be empty guid.", nameof(id));
+            }
+
+            if (string.IsNullOrWhiteSpace(sku))
+            {
+                throw new ArgumentException("Can not be blank.", nameof(sku));
+            }
+
+            if (unitPrice < 0)
+            {
+                throw new ArgumentException("Can not be negative.", nameof(unitPrice));
+            }
+
+            Id = id;
+            SKU = sku;
+            UnitPrice = unitPrice;
+        }
+        public Guid Id { get; }
+
+        public string SKU { get; }
+
+        public decimal UnitPrice { get; }
+    }
+}
+
